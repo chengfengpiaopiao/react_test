7f18e8cb08d702faf50d52460a4c37f26fec2a2b
chengfengpiaopiao <869010486@qq.com>
2020-03-16 14:30:53 +0800

    jquery css模块化调用

--- src/App.js
+++ src/App.js
@@ -3,26 +3,47 @@ import {
     Link,
 } from 'react-router-dom'
 import styles from  './App.css';
-function App() {
-  return (
-    <div>
-      <header >
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<p>*/}
-        {/*  Edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
-          <Link to="/style_simple">
+import $ from 'jquery'
+class App extends React.Component{
+    componentDidMount() {
+        $("#show_ewm_img").mouseenter(function () {
 
-              <div className={styles.App}>this Project is for React</div>
+            var str = "#" +styles.mouse_on_wrap;
+            $(".App__mouse_on_wrap__1kp4J").addClass(styles.mouse_bg)
+        });
+    }
 
-          </Link>
-          <div className={styles.AppLink}>this Project is for React</div>
-          <div>
-              <div className={styles.Bg}></div>
-          </div>
-      </header>
-    </div>
-  );
+    render() {
+        return (
+            <div>
+                <header >
+                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                    {/*<p>*/}
+                    {/*  Edit <code>src/App.js</code> and save to reload.*/}
+                    {/*</p>*/}
+                    <Link to="/style_simple">
+
+                        <div className={styles.App}>this Project is for React</div>
+
+                    </Link>
+                    <div className={styles.AppLink}>this Project is for React</div>
+                    <div>
+                        <div className={styles.Bg}></div>
+                    </div>
+
+                    <div>测试JQuery</div>
+
+                    <h2 id="show_ewm_img">鼠标悬浮出现图片</h2>
+
+                    <div id="mouse_wrap" className={styles.mouse_on_wrap}>
+                        <img src="./public/banner1.jpg" width={100}/>
+
+                    </div>
+
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
